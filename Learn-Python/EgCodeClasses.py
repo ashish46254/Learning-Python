c1a1c49d722aa44a8285777e5615074dca4f29a8
ashish46254 <ashish_verma@outlook.com>
2017-09-01 13:39:14 -0500

    0.1

--- Learn-Python/EgCodeClasses.py
+++ Learn-Python/EgCodeClasses.py
@@ -1,25 +1,25 @@
 #! /usr/bin/python34
 
-class myClass():
-  def method1(self):
-    print ("myClass method1")
+class mainClass():
+    def method1(self):
+        print ("mainClass method1")
     
-  def method2(self, someString):
-    print ("myClass method2: " + someString)
+    def method2(self, someString):
+        print("mainClass method2: " + someString)
     
-class anotherClass(myClass):
-  def method2(self):
-    print ("anotherClass method2")
+class drivedClass(mainClass):
+    def method2(self):
+        print ("Class method2")
     
-  def method1(self):
-    myClass.method1(self);
-    print ("anotherClass method1")
+    def method1(self):
+        mainClass.method1(self);
+        print ("anotherClass method1")
       
 def main():
-  c = myClass()
-  c.method1()
-  c.method2("This is a string")
-  c2 = anotherClass()
-  c2.method1()
+    c = mainClass()
+    c.method1()
+    c.method2("This is a string")
+    c2 = drivedClass()
+    c2.method1()
   
 if __name__ == "__main__":   main()
